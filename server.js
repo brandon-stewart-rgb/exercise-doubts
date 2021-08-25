924591c7177205048304122fe738bbce85ef2d78
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-25 12:50:57 -0600

    heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
-
+  
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
